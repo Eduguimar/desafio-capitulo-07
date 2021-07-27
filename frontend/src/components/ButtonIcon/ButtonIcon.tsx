585183357ecbf4dc12374ae729b1c4ca2e8c41a7
Eduardo Guimaraes <eduguimara@gmail.com>
2021-07-27 19:04:21 -0300

    Home page created

---
+++ frontend/src/components/ButtonIcon/ButtonIcon.tsx
@@ -0,0 +1,14 @@
+import './buttonIcon.css';
+
+export default function ButtonIcon() {
+	return (
+		<div className="btn-container">
+			<button className="btn btn-second">
+				<h6>Ver Catálogo</h6>
+			</button>
+			<div className="btn-phrase">
+				<p>Comece agora a navegar</p>
+			</div>
+		</div>
+	);
+}
--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,5 +1,27 @@
 import './home.css';
+import { ReactComponent as CarImage } from '../../assets/images/main-car.svg';
+import ButtonIcon from '../../components/ButtonIcon/ButtonIcon';
 
 export default function Home() {
-	return <div>Hello BOys</div>;
+	return (
+		<div className="home-container">
+			<div className="base-card home-card">
+				<div className="home-content-container">
+					<div>
+						<h1>O carro perfeito para você</h1>
+						<p>
+							Conheça nossos carros e dê mais um passo na realização do seu
+							sonho
+						</p>
+					</div>
+				</div>
+				<div className="home-image-container">
+					<CarImage />
+				</div>
+			</div>
+			<div>
+				<ButtonIcon />
+			</div>
+		</div>
+	);
 }
